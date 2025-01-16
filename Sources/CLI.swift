8d165a36c4153a555c828c6768c1fc2c9f54b860
Jarrod Taylor <email@jarrodtaylor.me>
2025-01-16 01:40:24 -0500

    Adds CLI

--- Sources/CLI.swift
+++ Sources/CLI.swift
@@ -3,7 +3,40 @@ import Foundation
 
 @main
 struct CLI: ParsableCommand {
+  static let configuration = CommandConfiguration(
+    commandName: "alternator",
+    abstract: "Alternator builds static websites.",
+    discussion: "Visit https://alternator.sh to learn more.",
+    version: "2.0.0")
+  
+  @Argument(help: "Path to your source directory.")
+  var source: String = "."
+
+  @Argument(help: "Path to your target directory.")
+  var target: String = "<source>/_build"
+  
+  @Option(name: .shortAndLong, help: "Port for the localhost server.")
+  var port: UInt16?
+  
   mutating func run() throws {
-    print("Hello, world!")
+    if target == "<source>/_build" { target = "\(source)/_build" }
+    
+    log("TODO: build project")
+    
+    if let port = port {
+      log("TODO: run watcher and server on port \(port)")
+    }
+  }
+}
+
+nonisolated(unsafe) var standardError = FileHandle.standardError
+
+func log(_ message: String) {
+  print(message, to: &standardError)
+}
+
+extension FileHandle: @retroactive TextOutputStream {
+  public func write(_ message: String) {
+    write(message.data(using: .utf8)!)
   }
 }
